fccba55c299479acb3da0984d7b1741a696143b3
Eduardo Stein Brito <eduardobrito14@gmail.com>
2016-12-15 11:59:41 -0200

    MODIFY: Better UI and show details when clicked on Movie Title

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,11 +3,27 @@ app.controller('moviesCtrl', ['$scope','$http',function($scope, $http) {
     var obj = {content:null};
 
     $http.get("https://api.themoviedb.org/3/movie/upcoming?page=1&language=en-US&api_key=c5850ed73901b8d268d0898a8a9d8bff").success(function(data) {
-        // you can do some processing here
-        console.log(data);
-        obj.content = data;
         $scope.movies = data.results
     });
+
+    $scope.getGenre = function(movie){
+        $scope.currentMovieGenres = "No genre defined";
+        $http.get("https://api.themoviedb.org/3/genre/movie/list?api_key=c5850ed73901b8d268d0898a8a9d8bff").success(function(allGenres) {
+            $scope.currentMovieGenres = "";
+            angular.forEach(allGenres[0], function(genre, key) {
+                angular.forEach(movie.genre_ids, function(genreId, keyGenre) {
+                    console.log(genre);
+                    console.log(movie.genre_ids);
+                    if(genre.id == genreId){
+                        $scope.currentMovieGenres = $scope.currentMovieGenres + result.name;
+                    }
+
+                });
+            });
+
+        });
+
+    }
 
 
 }]);
